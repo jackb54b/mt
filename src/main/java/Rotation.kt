c335623f456808ee81866d34fed19be74b2c44e1
pav <ala32fala43@gmail.com>
2020-01-19 17:18:43 +0100

    rotation

---
+++ src/main/java/Rotation.kt
@@ -0,0 +1,18 @@
+//
+//class Rotation<T>(ts: Array<T>) {
+//
+//    fun <T> rotate(n: Int) : Array<T> {
+//        var result:Array<T> = emptyArray()
+//
+//        return result
+//    }
+//
+//}
+class Task<X : T?> {
+    fun a(): Array<T?> {
+        return arrayOfNulls(1)
+    }
+}
+
+open class T
+internal class Z : T()
